e00faf5f178e6a4980d08a41ab73dde014301391
thisiskcp <syauqireza28@gmail.com>
2024-12-26 23:13:37 +0700

    ta

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Navbar from "./Navbar";
+import HeroSection from "./HeroSection";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+    </div>
+  );
+}
